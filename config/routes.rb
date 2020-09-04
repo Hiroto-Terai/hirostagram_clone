79e8b7d33439e994a895d62173ad863343646b94
Hiroto-Terai <hiro08051610@gmail.com>
2020-09-04 22:05:53 +0900

    投稿の詳細ページを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
 
   get '/users/:id', to:'users#show', as:'user'
 
-  get '/posts/new', to:'posts#new'
-  get '/posts', to:'posts#index'
-  post '/posts', to:'posts#create'
-  post '/posts/:post_id/photos', to:'photos#create', as:'post_photos'
+  resources :posts, only: %i(index new create show) do
+    resources :photos, only: %i(create)
+  end
 
   
 end
